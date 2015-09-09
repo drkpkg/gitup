919324d6b2cc806804f99c29ce148ae376aabee2
Federico Damián <federicodamians@gmail.com>
2015-09-09 01:19:10 -0300

    Fix

---
+++ gitup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# GitUp: a simple GitHub uploader (Bash scripting excercise)
+# Federico Damián Schonborn (feskyde)
+# 2015
+
+git add .
+echo "Type your commit message here:"
+read commitm
+git commit -m "$commitm"
+git push -u origin master
